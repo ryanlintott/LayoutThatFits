fdf6837356ce02e64f6080f0e0f01bb264086e1e
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2022-06-09 20:10:54 -0400

    Initial Commit

---
+++ LayoutThatFits/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LayoutThatFits
+//
+//  Created by Ryan Lintott on 2022-06-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutThatFits/LayoutThatFitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutThatFitsApp.swift
+//  LayoutThatFits
+//
+//  Created by Ryan Lintott on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct LayoutThatFitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
